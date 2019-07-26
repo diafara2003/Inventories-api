4b51446bbfcc1de004954e1d422d7b700d9d3da7
jhonnatan uruena <jaud_@hotmail.com>
2019-07-25 21:36:03 -0500

    :jau

--- DataAccessLayer/Persistencia/EntradaDTO.cs
+++ DataAccessLayer/Persistencia/EntradaDTO.cs
@@ -5,7 +5,7 @@ namespace DataAccessLayer {
 
     public class EntradaDTO {
         public int EnId { get; set; }
-        public string EnProveedor { get; set; }
+        public int EnProveedor { get; set; }
         public DateTime EnFecha { get; set; }
         public int? EnEstado { get; set; }
         public int? EnUsuarioCrea { get; set; }
@@ -15,5 +15,6 @@ namespace DataAccessLayer {
         public string EnObservacion { get; set; }
 
         public List<EntradaDetalleDTO> EntradaDetalle { get; set; }
+        public TerceroDTO terceroEntrada { get; set; }
     }
 }
--- LogicLayer/business/entradasBI.cs
+++ LogicLayer/business/entradasBI.cs
@@ -4,81 +4,99 @@ using System.Linq;
 using DataAccessLayer;
 using LogicLayerBusiness.Modelo;
 
-namespace LogicLayerBusiness {
+namespace LogicLayerBusiness
+{
 
-    public class EntradaBI {
+    public class EntradaBI
+    {
 
-        public IEnumerable<EntradaDTO> Get () {
-            IEnumerable<EntradaDTO> objResponse = new List<EntradaDTO> ();
+        public IEnumerable<EntradaDTO> Get()
+        {
+            IEnumerable<EntradaDTO> objResponse = new List<EntradaDTO>();
 
-            ConexionDTO cnn = new ConexionDTO ();
+            ConexionDTO cnn = new ConexionDTO();
             cnn.procedimiento = "GetEntradafiltros";
 
-            var dt = new Conexion ().ConsultarSPDT (cnn);
+            var dt = new Conexion().ConsultarSPDT(cnn);
 
-            if (dt.Rows.Count > 0) {
+            if (dt.Rows.Count > 0)
+            {
 
-                objResponse = Tool.DataTableToList<EntradaDTO> (dt);
+                objResponse = Tool.DataTableToList<EntradaDTO>(dt);
+
+                foreach (EntradaDTO item in objResponse)
+                {
+                    item.terceroEntrada=new TerceroBI().GetXId(item.EnProveedor);
+                }
 
             }
 
             return objResponse;
         }
 
-        public IEnumerable<EntradaDTO> GetState (int state = -1) {
-            IEnumerable<EntradaDTO> objResponse = new List<EntradaDTO> ();
+        public IEnumerable<EntradaDTO> GetState(int state = -1)
+        {
+            IEnumerable<EntradaDTO> objResponse = new List<EntradaDTO>();
 
-            ConexionDTO cnn = new ConexionDTO ();
+            ConexionDTO cnn = new ConexionDTO();
             cnn.procedimiento = "GetEntradafiltros";
-            cnn.parametros.Add ("@state", state);
+            cnn.parametros.Add("@state", state);
 
-            var dt = new Conexion ().ConsultarSPDT (cnn);
+            var dt = new Conexion().ConsultarSPDT(cnn);
 
-            if (dt.Rows.Count > 0) {
+            if (dt.Rows.Count > 0)
+            {
 
-                objResponse = Tool.DataTableToList<EntradaDTO> (dt);
+                objResponse = Tool.DataTableToList<EntradaDTO>(dt);
 
             }
 
             return objResponse;
         }
 
-        public EntradaDTO GetXId (int id) {
-            EntradaDTO objResponse = new EntradaDTO ();
+        public EntradaDTO GetXId(int id)
+        {
+            EntradaDTO objResponse = new EntradaDTO();
 
-            ConexionDTO cnn = new ConexionDTO ();
+            ConexionDTO cnn = new ConexionDTO();
             cnn.procedimiento = "GetEntradafiltros";
-            cnn.parametros.Add ("@id", id);
+            cnn.parametros.Add("@id", id);
 
-            var dt = new Conexion ().ConsultarSPDT (cnn);
+            var dt = new Conexion().ConsultarSPDT(cnn);
 
-            if (dt.Rows.Count > 0) {
-
-                objResponse = Tool.DataTableToList<EntradaDTO> (dt).FirstOrDefault ();
-                objResponse.EntradaDetalle = new EntradaDetalleBI ().GetxIdEntrada (id);
+            if (dt.Rows.Count > 0)
+            {
+                int id_tercero = (int)dt.Rows[0]["EnProveedor"];
+                objResponse = Tool.DataTableToList<EntradaDTO>(dt).FirstOrDefault();
+                objResponse.EntradaDetalle = new EntradaDetalleBI().GetxIdEntrada(id);
+                objResponse.terceroEntrada = new TerceroBI().GetXId(id_tercero);
 
             }
 
             return objResponse;
         }
 
-        public ResponseDTO CambiarEstado (AprobarEntradaDTO modelo) {
+        public ResponseDTO CambiarEstado(AprobarEntradaDTO modelo)
+        {
 
-            ResponseDTO objresponse = new ResponseDTO ();
+            ResponseDTO objresponse = new ResponseDTO();
 
-            try {
+            try
+            {
 
-                ConexionDTO cnn = new ConexionDTO ();
+                ConexionDTO cnn = new ConexionDTO();
                 cnn.procedimiento = "ActualizarEntradaEstado";
-                cnn.parametros.Add ("EnId", modelo.Identrada);
-                cnn.parametros.Add ("EnEstado", modelo.estado);
+                cnn.parametros.Add("EnId", modelo.Identrada);
+                cnn.parametros.Add("EnEstado", modelo.estado);
 
                 objresponse.codigo = 1;
                 objresponse.mensaje = string.Empty;
 
-                new Conexion ().ConsultarSPDT (cnn);
+                new Conexion().ConsultarSPDT(cnn);
 
-            } catch (System.Exception e) {
+            }
+            catch (System.Exception e)
+            {
 
                 objresponse.codigo = -1;
                 objresponse.mensaje = e.Message;
@@ -87,31 +105,37 @@ namespace LogicLayerBusiness {
             return objresponse;
         }
 
-        public ResponseDTO Update (EntradaDTO modelo) {
+        public ResponseDTO Update(EntradaDTO modelo)
+        {
 
-            ResponseDTO objresponse = new ResponseDTO ();
+            ResponseDTO objresponse = new ResponseDTO();
 
             objresponse.codigo = modelo.EnId;
             objresponse.mensaje = string.Empty;
 
-            ConexionDTO cnn = new ConexionDTO ();
+            ConexionDTO cnn = new ConexionDTO();
             cnn.procedimiento = "ActualizarEntrada";
-            cnn.parametros.Add ("EnId", modelo.EnId);
-            cnn.parametros.Add ("EnProveedor", modelo.EnProveedor);
-            cnn.parametros.Add ("EnObservacion", modelo.EnObservacion);
-            cnn.parametros.Add ("EnUsuarioModifica", modelo.EnUsuarioModifica);
+            cnn.parametros.Add("EnId", modelo.EnId);
+            cnn.parametros.Add("EnProveedor", modelo.EnProveedor);
+            cnn.parametros.Add("EnObservacion", modelo.EnObservacion);
+            cnn.parametros.Add("EnUsuarioModifica", modelo.EnUsuarioModifica);
 
-            try {
-                var dt = new Conexion ().ConsultarSPDT (cnn);
+            try
+            {
+                var dt = new Conexion().ConsultarSPDT(cnn);
 
-                if (modelo.EntradaDetalle != null) {
-                    foreach (var item in modelo.EntradaDetalle) {
+                if (modelo.EntradaDetalle != null)
+                {
+                    foreach (var item in modelo.EntradaDetalle)
+                    {
                         item.EndDetEntradaId = modelo.EnId;
-                        new EntradaDetalleBI ().Insert (item);
+                        new EntradaDetalleBI().Insert(item);
                     }
                 }
 
-            } catch (System.Exception e) {
+            }
+            catch (System.Exception e)
+            {
                 objresponse.codigo = -1;
                 objresponse.mensaje = e.Message;
 
@@ -121,31 +145,37 @@ namespace LogicLayerBusiness {
 
         }
 
-        public ResponseDTO Insert (EntradaDTO modelo) {
+        public ResponseDTO Insert(EntradaDTO modelo)
+        {
 
-            ResponseDTO objresponse = new ResponseDTO ();
-            ConexionDTO cnn = new ConexionDTO ();
+            ResponseDTO objresponse = new ResponseDTO();
+            ConexionDTO cnn = new ConexionDTO();
 
             objresponse.mensaje = "Se creó la entrada correctamente";
 
-            try {
+            try
+            {
                 cnn.procedimiento = "InsertarEntrada";
-                cnn.parametros.Add ("EnProveedor", modelo.EnProveedor);
-                cnn.parametros.Add ("EnUsuarioCrea", modelo.EnUsuarioCrea);
-                cnn.parametros.Add ("EnObservacion", modelo.EnObservacion);
+                cnn.parametros.Add("EnProveedor", modelo.EnProveedor);
+                cnn.parametros.Add("EnUsuarioCrea", modelo.EnUsuarioCrea);
+                cnn.parametros.Add("EnObservacion", modelo.EnObservacion);
 
-                var dt = new Conexion ().ConsultarSPDT (cnn);
+                var dt = new Conexion().ConsultarSPDT(cnn);
 
-                objresponse.codigo = (int) dt.Rows[0]["id"];
+                objresponse.codigo = (int)dt.Rows[0]["id"];
 
-                if (modelo.EntradaDetalle != null) {
-                    foreach (var item in modelo.EntradaDetalle) {
+                if (modelo.EntradaDetalle != null)
+                {
+                    foreach (var item in modelo.EntradaDetalle)
+                    {
                         item.EndDetEntradaId = objresponse.codigo;
-                        new EntradaDetalleBI ().Insert (item);
+                        new EntradaDetalleBI().Insert(item);
                     }
                 }
 
-            } catch (System.Exception e) {
+            }
+            catch (System.Exception e)
+            {
 
                 objresponse.codigo = -1;
                 objresponse.mensaje = e.Message;
@@ -154,18 +184,22 @@ namespace LogicLayerBusiness {
             return objresponse;
         }
 
-        public ResponseDTO Delete (int id) {
+        public ResponseDTO Delete(int id)
+        {
 
-            ResponseDTO objresponse = new ResponseDTO ();
-            ConexionDTO cnn = new ConexionDTO ();
+            ResponseDTO objresponse = new ResponseDTO();
+            ConexionDTO cnn = new ConexionDTO();
             cnn.procedimiento = "EliminarEntradas";
-            cnn.parametros.Add ("id", id);
+            cnn.parametros.Add("id", id);
             objresponse.codigo = 1;
             objresponse.mensaje = string.Empty;
 
-            try {
-                var dt = new Conexion ().ConsultarSPDT (cnn);
-            } catch (System.Exception e) {
+            try
+            {
+                var dt = new Conexion().ConsultarSPDT(cnn);
+            }
+            catch (System.Exception e)
+            {
                 objresponse.codigo = -1;
                 objresponse.mensaje = e.Message;
 
